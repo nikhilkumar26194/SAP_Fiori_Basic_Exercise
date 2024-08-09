b1dea67943217fa245ddebf3e932ebacb1ee4885
Nikhil kumar <nikhilkumar26194sap@gmail.com>
2024-08-09 05:26:33 +0000

    add list and created model

--- project1/webapp/controller/GOPRO.controller.js
+++ project1/webapp/controller/GOPRO.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
+function (Controller ,JSONModel) {
     "use strict";
 
     return Controller.extend("gopro1.project1.controller.GOPRO", {
         onInit: function () {
+            var oModel = JSONModel( );
+            this.getView().setModel(oModel);
               
         }
     });
